88d1d0c599fd37db2b4e7de8dc1f006f3de8c7bf
Lyllys Galhardo <lyllysgalhardo@gmail.com>
2021-06-08 19:41:28 -0300

    Order,Instant, ISO8601

---
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -0,0 +1,92 @@
+package com.educandoweb.course.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_order")
+public class Order implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private Instant momento;
+	
+	@ManyToOne // um usuário pode fazer muitos pedidos;
+	@JoinColumn(name = "cliente_id") // nome da chave estrageira que vai ter lá no BD;
+	private User cliente;
+	
+	public Order() {
+		
+	}
+
+	public Order(Long id, Instant momento, User cliente) {
+		super();
+		this.id = id;
+		this.momento = momento;
+		this.cliente = cliente;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Instant getMomento() {
+		return momento;
+	}
+
+	public void setMomento(Instant momento) {
+		this.momento = momento;
+	}
+
+	public User getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(User cliente) {
+		this.cliente = cliente;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+	
+
+}
